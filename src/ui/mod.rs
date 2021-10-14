1fe90f385ae562f7616eacee2796b5c2d47743b5
Max Jöhnk <maxjoehnk@gmail.com>
2021-10-14 19:53:48 +0200

    feat(ui): improve layout and styling of todos

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,5 +1,5 @@
-use druid::{Env, Event, EventCtx, FontDescriptor, FontFamily, FontWeight, Widget, WidgetExt, UnitPoint};
-use druid::widget::{Checkbox, Controller, Flex, Label, List, Scroll};
+use druid::{Env, Event, EventCtx, FontDescriptor, FontFamily, FontWeight, Widget, WidgetExt, Color, Insets, UnitPoint};
+use druid::widget::{Controller, Flex, Label, List, Scroll, CrossAxisAlignment, LineBreaking, Either};
 
 use crate::models::{Todo, TodoList, TodoProvider};
 
@@ -23,8 +23,7 @@ impl<W: Widget<TodoList>> Controller<TodoList, W> for Sidenotes {
 
 pub fn ui_builder() -> impl Widget<TodoList> {
     let list = List::new(provider_builder)
-        .lens(TodoList::providers)
-        .expand_width();
+        .lens(TodoList::providers);
     let list = Scroll::new(list).vertical();
 
     list
@@ -35,31 +34,48 @@ fn provider_builder() -> impl Widget<TodoProvider> {
     let font = FontDescriptor::new(FontFamily::SYSTEM_UI)
         .with_size(18.0)
         .with_weight(FontWeight::BOLD);
-    let header = Label::new(|item: &TodoProvider, _env: &_| item.name.clone()).with_font(font);
+    let header = Label::new(|item: &TodoProvider, _env: &_| item.name.clone()).with_font(font)
+        .align_horizontal(UnitPoint::CENTER);
     let todos = List::new(todo_builder)
         .lens(TodoProvider::items);
 
     Flex::column()
         .with_child(header)
         .with_child(todos)
-        .align_vertical(UnitPoint::CENTER)
+        .cross_axis_alignment(CrossAxisAlignment::Start)
+        .padding(4.0)
 }
 
 fn todo_builder() -> impl Widget<Todo> {
-    let title = Label::new(|item: &Todo, _env: &_| item.title.clone());
-    let checkbox = Checkbox::new(String::new())
-        .disabled_if(|_, _| true)
-        .lens(Todo::completed);
-    let header = Flex::row()
-        .with_child(checkbox)
-        .with_child(title);
+    let with_state = Label::new(|todo: &Todo, _env: &_| todo.state.clone().unwrap_or_default())
+        .with_text_color(Color::BLACK)
+        .padding(2.0)
+        .background(Color::from_hex_str("a3be8c").unwrap())
+        .rounded(2.0);
+    let with_state = Flex::column()
+        .cross_axis_alignment(CrossAxisAlignment::Start)
+        .with_child(title_builder())
+        .with_spacer(4.0)
+        .with_child(with_state);
+    let without_state = Flex::column()
+        .cross_axis_alignment(CrossAxisAlignment::Start)
+        .with_child(title_builder());
 
-    let state = Label::new(|item: &Todo, _env: &_| item.state.clone().unwrap_or_default());
+    let state = Either::new(
+        |todo: &Todo, _: &_| todo.state.is_some(),
+        with_state,
+        without_state
+    );
 
-    Flex::column()
-        .with_child(header)
-        .with_child(state)
-        .padding(8.)
-        .align_left()
+    state
+        .padding(4.0)
+        .background(Color::rgba8(0, 0, 0, 32))
+        .rounded(2.0)
+        .padding(Insets::uniform_xy(0., 2.))
         .expand_width()
+}
+
+fn title_builder() -> impl Widget<Todo> {
+    Label::new(|item: &Todo, _env: &_| item.title.clone())
+        .with_line_break_mode(LineBreaking::WordWrap)
 }
