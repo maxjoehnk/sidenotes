050776c7f9c5239583c6a89ecd0da41bede4b2a7
Max Jöhnk <maxjoehnk@gmail.com>
2021-11-15 09:44:58 +0100

    feat(providers/github): allow configuration of custom search query

--- src/providers/github.rs
+++ src/providers/github.rs
@@ -1,29 +1,33 @@
-use super::Provider;
-use crate::models::Todo;
-use crate::rich_text::Markdown;
 use async_compat::CompatExt;
 use druid::im::Vector;
 use futures::future::BoxFuture;
 use futures::FutureExt;
 use octorust::auth::Credentials;
-use octorust::types::{
-    IssuesListState, Order, PullRequestReviewData, PullRequestSimple, PullsListSort,
-};
 use octorust::Client;
+use octorust::types::{IssueSearchResultItem, IssuesListState, Order, PullRequestReviewData, PullRequestSimple, PullsListSort, SearchIssuesPullRequestsSort, SimpleUser};
 use serde::Deserialize;
+
+use crate::models::Todo;
+use crate::rich_text::Markdown;
+
+use super::Provider;
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct GithubConfig {
     #[serde(default)]
     name: Option<String>,
     token: String,
+    #[serde(default)]
     repos: Vec<String>,
+    #[serde(default)]
+    query: Option<String>,
 }
 
 pub struct GithubProvider {
     name: Option<String>,
     client: Client,
     repos: Vec<(String, String)>,
+    query: Option<String>,
 }
 
 impl GithubProvider {
@@ -42,10 +46,25 @@ impl GithubProvider {
                     (parts[0].into(), parts[1].into())
                 })
                 .collect(),
+            query: config.query,
         })
     }
 
     async fn fetch_todos(&self) -> anyhow::Result<Vector<Todo>> {
+        tracing::info!("Fetching Github Todos...");
+        let (lhs, rhs): (Vector<Todo>, Vector<Todo>) = futures::future::try_join(
+            self.fetch_repo_pull_requests(),
+            self.search_issues_and_prs()
+        ).await?;
+        let mut todos = lhs;
+        todos.append(rhs);
+
+        tracing::info!("Fetched {} Github Todos", todos.len());
+
+        Ok(todos)
+    }
+
+    async fn fetch_repo_pull_requests(&self) -> anyhow::Result<Vector<Todo>> {
         tracing::info!("Fetching Github PRs...");
         let mut todos = Vector::new();
         for (owner, repo) in self.repos.iter() {
@@ -82,8 +101,49 @@ impl GithubProvider {
         Ok(todos)
     }
 
-    fn get_pr_state(pr: &PullRequestSimple, reviews: &[PullRequestReviewData]) -> String {
-        if pr.draft {
+    async fn search_issues_and_prs(&self) -> anyhow::Result<Vector<Todo>> {
+        if let Some(query) = self.query.as_ref() {
+            tracing::info!("Searching for Github Issues and PRs...");
+            let mut todos = Vector::new();
+            let res = self.client.search()
+                .issues_and_pull_requests(query, SearchIssuesPullRequestsSort::Created, Order::Asc, 20, 0)
+                .await?;
+            for item in res.items {
+                let state = if item.pull_request.is_some() {
+                    if let Some(repository_url) = item.repository_url.as_ref().and_then(|url| url.path_segments()) {
+                        let paths = repository_url.skip(1).collect::<Vec<_>>();
+
+                        let reviews = self
+                            .client
+                            .pulls()
+                            .list_all_reviews(paths[0], paths[1], item.number)
+                            .await?;
+                        Some(Self::get_pr_state(&item, &reviews))
+                    }else {
+                        None
+                    }
+                }else {
+                    Some(item.state)
+                };
+                todos.push_back(Todo {
+                    title: format!("#{} - {}", item.number, item.title),
+                    state,
+                    author: item.user.map(|user| user.name),
+                    body: Some(Markdown(item.body).into()),
+                    link: item.html_url.map(|url| url.to_string()),
+                });
+            }
+
+            tracing::info!("Found {} Github Issues and PRs", todos.len());
+
+            Ok(todos)
+        }else {
+            Ok(Default::default())
+        }
+    }
+
+    fn get_pr_state(pr: &impl GithubIssueOrPullRequest, reviews: &[PullRequestReviewData]) -> String {
+        if pr.is_draft() {
             return "Draft".into();
         }
         if reviews
@@ -111,5 +171,30 @@ impl Provider for GithubProvider {
 
     fn fetch_todos(&self) -> BoxFuture<anyhow::Result<Vector<Todo>>> {
         self.fetch_todos().compat().boxed()
+    }
+}
+
+trait GithubIssueOrPullRequest {
+    fn is_draft(&self) -> bool;
+    fn user(&self) -> Option<&SimpleUser>;
+}
+
+impl GithubIssueOrPullRequest for PullRequestSimple {
+    fn is_draft(&self) -> bool {
+        self.draft
+    }
+
+    fn user(&self) -> Option<&SimpleUser> {
+        self.user.as_ref()
+    }
+}
+
+impl GithubIssueOrPullRequest for IssueSearchResultItem {
+    fn is_draft(&self) -> bool {
+        self.draft
+    }
+
+    fn user(&self) -> Option<&SimpleUser> {
+        self.user.as_ref()
     }
 }
