54a8fe34e9e1d8b6513f27d5824c9e490b6d696b
Max Jöhnk <maxjoehnk@gmail.com>
2022-03-09 10:02:37 +0100

    fix(meeting-card): time until meeting doesn't update

--- src/ui/widgets/clickable_area.rs
+++ src/ui/widgets/clickable_area.rs
@@ -4,11 +4,12 @@ use druid::{Cursor, Data, Env, Event, EventCtx, Widget};
 pub struct ClickableArea;
 
 impl<T: Data, W: Widget<T>> Controller<T, W> for ClickableArea {
-    fn event(&mut self, _: &mut W, ctx: &mut EventCtx, _: &Event, _: &mut T, _: &Env) {
+    fn event(&mut self, child: &mut W, ctx: &mut EventCtx, event: &Event, data: &mut T, env: &Env) {
         if ctx.is_hot() {
             ctx.set_cursor(&Cursor::Pointer)
         } else {
             ctx.set_cursor(&Cursor::Arrow)
         }
+        child.event(ctx, event, data, env)
     }
 }
